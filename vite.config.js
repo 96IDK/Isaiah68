3ce6696ba6d15f809220b92aa723e49ead89607b
96IDK <id.kangalee@gmail.com>
2026-03-09 12:44:06 -0700

    Build site to docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
-   import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react'
 
-   export default defineConfig({
-     base: '/Isaiah68/',
-     plugins: [react()],
-   })
+export default defineConfig({
+  base: '/Isaiah68/',
+  plugins: [react()],
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true
+  }
+})
